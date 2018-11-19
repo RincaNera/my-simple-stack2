7599821b4c68a8319618787372c33787a3c38fbe
RincaNera <yoan.sanna@netc.fr>
2018-11-19 02:26:39 +0100

    First commit

---
+++ src/main/java/deqo/ysan/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.ysan.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
